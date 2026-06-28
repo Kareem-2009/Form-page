66d8ccf7f87d9ea3f37770b7c8b82db918c786b4
Kareem-2009 <nemrkareem6@gmail.com>
2026-06-28 19:49:20 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    alert('Logged in successfully!');
+});
